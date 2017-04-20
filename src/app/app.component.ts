810baf378002d8e0844029502966ad2d3f2447fe
wajid-snapad <wajid.khilji@snapad.net>
2017-04-20 16:10:24 +0300

    2nd comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app works!';
+  selectedLanguage: string;
+  wolves = 0;
+  gender = 'f';
+  fly = true;
+  logo = 'https://angular.io/resources/images/logos/angular/angular.png';
+  inc(i: number) {
+    this.wolves = Math.min(5, Math.max(0, this.wolves + i));
+  }
+  male()   { this.gender = 'm'; }
+  female() { this.gender = 'f'; }
+
+  ngOnInit(){
+    this.selectedLanguage = localStorage.getItem('locale') as string;
+  }
+
+  languageChanged(value){
+    localStorage.setItem('locale', value);
+    location.reload();
+  }
 }
--- src/app/i18n.provider.ts
+++ src/app/i18n.provider.ts
@@ -1,3 +1,53 @@
 /**
  * Created by wajidkhilji on 20/04/2017.
  */
+import { TRANSLATIONS, TRANSLATIONS_FORMAT, LOCALE_ID } from '@angular/core';
+import { Observable } from "rxjs/Rx";
+import {objectify} from "tslint/lib/utils";
+import { environment } from 'environments/environment';
+export function getTranslationProviders(): Promise<Object[]> {
+
+
+  // Get the locale id from the global
+  let langStore = localStorage.getItem('locale') as string;
+  const locale = langStore ? langStore: environment.locale;
+  // return no providers if fail to get translation file for locale
+  const noProviders: Object[] = [];
+  // No locale or U.S. English: no translation providers
+  if (!locale || locale === environment.locale) {
+    return Promise.resolve(noProviders);
+  }
+  // Ex: 'locale/messages.fr.xlf`
+  const translationFile = `./src/locale/messages.${locale}.xlf`;
+  return getTranslations(translationFile)
+    .then((translations: string) => [
+      { provide: TRANSLATIONS, useValue: translations },
+      { provide: TRANSLATIONS_FORMAT, useValue: 'xlf' },
+      { provide: LOCALE_ID, useValue: locale }
+    ])
+    .catch(() => noProviders); // ignore if file not found
+}
+
+function getTranslations(file: string) {
+// changes Start here
+  let text = "";
+  let fileRequest = new XMLHttpRequest();
+
+  fileRequest.onreadystatechange = function () {
+    if (fileRequest.readyState === 4) {
+      if (fileRequest.status === 200 || fileRequest.status == 0) {
+        text = fileRequest.responseText;
+      }
+    }
+  };
+
+  fileRequest.onerror = function (err) {
+    console.log(err);
+  };
+  fileRequest.open("GET", file, false);
+
+
+  fileRequest.send();
+  let observable = Observable.of(text);
+  return observable.toPromise();
+}
